21b8c4f3089cc2d1df1ebd095ca5c7796c583a12
nicolas-baranovskiy <nicolas.baranovskiy@gmail.com>
2022-08-04 14:22:01 +0300

    feat: index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let openBracket = [];
+  let closeBracket = [];
+  let stack = [];
+  let topElement;
+  
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    openBracket.push(bracketsConfig[i][0]);
+    closeBracket.push(bracketsConfig[i][1]);
+  }
+  
+  for (let i = 0; i < str.length; i++) {
+    for(let n = 0; n < bracketsConfig.length; n++){
+      if (str[i] ===openBracket[n] && str[i] === closeBracket[n]) {
+        if (!stack.includes(str[i])) {
+        stack.push(str[i])
+         topElement = stack[stack.length - 1];
+      } else{
+        stack.pop()
+        topElement = stack[stack.length - 1];
+      }
+      } else if (str[i] === openBracket[n]) {
+        stack.push(str[i])
+         topElement = stack[stack.length - 1];
+      } else if (str[i] === closeBracket[n]) {
+        
+        if (stack.length === 0) {
+          return false;
+        } 
+        
+        if (topElement === openBracket[n]) {
+          stack.pop()
+          topElement = stack[stack.length - 1];
+        } else {
+          return false;
+        }
+      }
+    } 
+   }
+  
+  return stack.length === 0;
 }
